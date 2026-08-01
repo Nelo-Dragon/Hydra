5e09e52955f3ada44958a4cb9fc30e57820327b5
Aretē <nelohildebrant@gmail.com>
2026-07-31 22:55:41 -0400

    got rid of debug

--- src/Runtime/runtime_commands.cpp
+++ src/Runtime/runtime_commands.cpp
@@ -421,16 +421,9 @@ Runtime::IsLoop Runtime::EndLoop(const Command& cmd, const IsLoop& isLoop) {
         return curLoop;
     }
 
-    std::cout << "ENDLOOP: "
-        << "line=" << curLoop.lineN.back()
-        << " iterations=" << curLoop.iteration.back()
-        << "\n";
-    
     curLoop.iteration.back() -= 1;
 
     if (curLoop.iteration.back() <= 0) {
-
-        std::cout << "POPPING LOOP\n";
 
         curLoop.iteration.pop_back();
         curLoop.lineN.pop_back();
